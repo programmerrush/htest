9edaab3224b0d26cedbba3216cbdeb9acb6177dd
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2018-06-08 21:48:36 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
+require_once "/vendor/autoload.php";
 
    // connect to mongodb
    $m = new MongoClient("mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5");
