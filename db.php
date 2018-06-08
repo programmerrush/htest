426c14335171103dd7dc508f507e55c98bc8bf5e
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2018-06-08 22:04:32 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 require_once "vendor/autoload.php";
 
    // connect to mongodb
-   $m = new MongoClient("mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5");
+   $m = new MongoDB\Driver\Manager("mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5");
 	
    echo "Connection to database successfully";
    // select a database
