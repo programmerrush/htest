bc2be8bd463a8665669320327a7cc9c3e374bb36
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2018-06-08 21:16:58 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
    // connect to mongodb
-   $m = new MongoClient(mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5);
+   $m = new MongoClient("mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5");
 	
    echo "Connection to database successfully";
    // select a database
