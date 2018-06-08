82ff40d257ccf7246df3f8fe297029d036bef851
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2018-06-08 20:13:04 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
    // connect to mongodb
    $m = new MongoClient(
-mongodb://admin:adminqwe1@ds247330.mlab.com:47330/programmerrush5
+mongodb://ad:12345q@ds247330.mlab.com:47330/programmerrush5
 );
 	
    echo "Connection to database successfully";
